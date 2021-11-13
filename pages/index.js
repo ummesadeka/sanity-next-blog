cb6ec8d68b57a213b04c46cb4174c0984a8307e6
ummesadeka <sadekaahmed53@gmail.com>
2021-11-13 21:51:52 +0600

    sanity next blog

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="container">
+    <Head>
+      <title>Sanity.io and Next JS Blog</title>
+      <meta
+        name="description"
+        content="Created by sabir Hussain www.isabir.dev"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main className={styles.main}>
+      <h1 className={styles.title}>
+        Welcome to <br /> sanity.io and Next.js Blog 
+      </h1>
+
+      <p className={styles.description}>
+       <Link href="/blog">Go to Blog</Link>
+      </p>
+    </main>
+
+  </div>
+  )
+}
---
+++ sanityClient.js
@@ -0,0 +1,4 @@
+export const sanityClient = {
+    projectId: 'tplrm0oh', //Replace this with your project ID
+    dataset:'production'
+}
